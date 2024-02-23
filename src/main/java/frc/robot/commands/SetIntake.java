b8245e4be24d39c79fb20f57f6e7f4a3a4349d2c
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2024-02-22 17:08:35 -0700

    delete test class

--- src/main/java/frc/robot/commands/SetIntake.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
-
-public class SetIntake extends CommandBase{
-    private final Intake intake;
-    private final boolean intaking;
-    
-}
