c12c7fefd9fdb7dcd4183d1effecc14a35eacd91
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2024-02-28 02:31:31 -0700

    vendor updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "main2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "a97928e1fa2759c610e5b94e6c4442ee2baecf83";
-  public static final String GIT_DATE = "2024-02-27 17:48:02 MST";
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "3d8e9008b069e60e7f953b3d9be883c6f1513617";
+  public static final String GIT_DATE = "2024-02-28 01:26:25 MST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-02-27 23:34:28 MST";
-  public static final long BUILD_UNIX_TIME = 1709102068623L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-02-28 02:27:50 MST";
+  public static final long BUILD_UNIX_TIME = 1709112470530L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
