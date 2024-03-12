2180d4c47cfca750108dbd02f2eabef20c29449a
ojlennon <owenjlennon@gmail.com>
2024-03-12 09:07:22 -0600

    Formatted

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,17 +15,22 @@ import edu.wpi.first.units.Units;
 import frc.robot.subsystems.shooter.Shooter.State;
 
 /**
- * Place to hold robot-wide numerical or boolean constants. This class should not be used for any
- * other purpose. All constants should be declared globally (i.e. public static). Do not put
+ * Place to hold robot-wide numerical or boolean constants. This class should
+ * not be used for any
+ * other purpose. All constants should be declared globally (i.e. public
+ * static). Do not put
  * anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static final boolean tuningMode = true;
+
   public static final class IDs {
-    
+
     // Tuner X
     public static final int pigeon = 0;
     public static final int swerveDriveTalon0 = 1;
@@ -51,13 +56,14 @@ public final class Constants {
     // REV Hardware Client
     public static final int flywheelLeft = 19;
     public static final int flywheelRight = 20;
-    
-    //PWF Config Page 10.49.44.2:5812
+
+    // PWF Config Page 10.49.44.2:5812
     public static final int intakesensor = 1;
     public static final int shootersensor = 2;
     public static final int ampsensor = 3;
-    
+
   }
+
   public static final Mode currentMode = Mode.REAL;
 
   public static enum Mode {
@@ -71,35 +77,34 @@ public final class Constants {
     REPLAY
   }
 
+  public static final List<Entry<Measure<Distance>, State>> SHOOTER_MAP = Arrays.asList(
+      Map.entry(Units.Meters.of(0), new State(2500, 0)),
+      Map.entry(Units.Meters.of(1.215), new State(3300, 0)),
+      Map.entry(Units.Meters.of(1.697), new State(3600, 0.095)),
+      Map.entry(Units.Meters.of(2.000), new State(3700, 0.133)),
+      Map.entry(Units.Meters.of(2.709), new State(3700, 0.206)),
+      Map.entry(Units.Meters.of(3.596), new State(3900, 0.242)),
+      Map.entry(Units.Meters.of(3.620), new State(3910, 0.248)));
 
-  public static final List<Entry<Measure<Distance>,State>> SHOOTER_MAP = Arrays.asList(
-    Map.entry(Units.Meters.of(0), new State(2500, 0)),
-     Map.entry(Units.Meters.of(1.215), new State(3300, 0)),
-      Map.entry(Units.Meters.of(1.697), new State(3600,0.095)),
-      Map.entry(Units.Meters.of(2.000), new State(3700,0.133)),
-      Map.entry(Units.Meters.of(2.709), new State(3700,0.206)),
-      Map.entry(Units.Meters.of(3.596), new State(3900,0.242)),
-      Map.entry(Units.Meters.of(3.620), new State(3910,0.248)));
-
-      //Map.entry(Units.Meters.of(3.596), new State(3900,0.255)));
+  // Map.entry(Units.Meters.of(3.596), new State(3900,0.255)));
 
   public static final double AprilTagHeights[] = {
-    53.38,
-    53.38,
-    57.13,
-    57.13,
-    53.38,
-    53.38,
-    57.13,
-    57.13,
-    53.38,
-    53.38,
-    52,
-    52,
-    52,
-    52,
-    52,
-    52
+      53.38,
+      53.38,
+      57.13,
+      57.13,
+      53.38,
+      53.38,
+      57.13,
+      57.13,
+      53.38,
+      53.38,
+      52,
+      52,
+      52,
+      52,
+      52,
+      52
   };
 
   public class XboxConstants {
@@ -140,9 +145,9 @@ public final class Constants {
   }
 
   /**
-     * Gotten from here
-     * https://firstfrc.blob.core.windows.net/frc2024/FieldAssets/2024LayoutMarkingDiagram.pdf
-     */
+   * Gotten from here
+   * https://firstfrc.blob.core.windows.net/frc2024/FieldAssets/2024LayoutMarkingDiagram.pdf
+   */
   public static class AprilTags {
     public static final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo
         .loadAprilTagLayoutField();
@@ -164,11 +169,11 @@ public final class Constants {
     public static int BLUE_STAGE_TOP = 15;
     public static int BLUE_STAGE_BOTTOM = 16;
   }
-    public static Pose2d mirrorPose(Pose2d bluePose) {
-        return new Pose2d(
-                Constants.AprilTags.aprilTagFieldLayout.getFieldLength() - bluePose.getX(),
-                bluePose.getY(),
-                Rotation2d.fromRadians(Math.PI - bluePose.getRotation().getRadians()));
-    }
-}
 
+  public static Pose2d mirrorPose(Pose2d bluePose) {
+    return new Pose2d(
+        Constants.AprilTags.aprilTagFieldLayout.getFieldLength() - bluePose.getX(),
+        bluePose.getY(),
+        Rotation2d.fromRadians(Math.PI - bluePose.getRotation().getRadians()));
+  }
+}
