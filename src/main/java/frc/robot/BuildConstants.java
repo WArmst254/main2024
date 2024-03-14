77a8b618da1febc8dc8cb79dfecc9cbb46673db1
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2024-03-13 23:00:02 -0600

    drive practice

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "main2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 71;
-  public static final String GIT_SHA = "9c9c854516a772ec4fc0cff51be2b75b4642d184";
-  public static final String GIT_DATE = "2024-03-12 12:08:52 MDT";
+  public static final int GIT_REVISION = 72;
+  public static final String GIT_SHA = "d44e658fe5581836a9b31eff10a8d49a1853e987";
+  public static final String GIT_DATE = "2024-03-13 18:05:44 MDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-03-13 18:01:17 MDT";
-  public static final long BUILD_UNIX_TIME = 1710374477270L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-13 18:07:45 MDT";
+  public static final long BUILD_UNIX_TIME = 1710374865294L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
