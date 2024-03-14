7735b7e4acf6691e374319ea28b475df74685831
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2024-03-13 23:00:32 -0600

    drive practice

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "d44e658fe5581836a9b31eff10a8d49a1853e987";
   public static final String GIT_DATE = "2024-03-13 18:05:44 MDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-03-13 18:07:45 MDT";
-  public static final long BUILD_UNIX_TIME = 1710374865294L;
+  public static final String BUILD_DATE = "2024-03-13 22:59:20 MDT";
+  public static final long BUILD_UNIX_TIME = 1710392360636L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
