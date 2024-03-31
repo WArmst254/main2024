7b004eee10bb2d0619de605e681880111e6c31d2
Wyatt Armstrong <120539174+WyattArmstrongWA@users.noreply.github.com>
2024-03-30 22:09:01 -0600

    CO code?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "main2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 90;
-  public static final String GIT_SHA = "84eefec269989d3ffffd7535b359e9be52f70ad2";
-  public static final String GIT_DATE = "2024-03-24 17:45:27 MDT";
+  public static final int GIT_REVISION = 91;
+  public static final String GIT_SHA = "d932d2703d60ca0fa0661b59a8a51f142abfc790";
+  public static final String GIT_DATE = "2024-03-28 22:35:33 MDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-03-28 20:54:01 MDT";
-  public static final long BUILD_UNIX_TIME = 1711680841009L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-29 22:09:05 MDT";
+  public static final long BUILD_UNIX_TIME = 1711771745530L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
